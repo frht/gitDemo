f8e467cf5db608efee0b1c90b11f6d7417dc5897
frht <karakasferhat05@gmail.com>
2020-08-01 11:27:05 -0400

    for loop added

--- src/day01/helloGit.java
+++ src/day01/helloGit.java
@@ -3,5 +3,8 @@ package day01;
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        for (int i=0;i<5;i++){
+            System.out.println(i);
+        }
     }
 }
